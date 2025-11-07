c10ee0b5736d377f6bba45e6f9ed04b9bb7f37be
ali-imtiyazkhan <itzkhantijara@gmail.com>
2025-11-08 03:03:52 +0530

    full code 2

--- client/src/app/account/page.tsx
+++ client/src/app/account/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { Badge } from "../../components/ui/badge";
+import { Button } from "../../components/ui/button";
+import { Card, CardContent } from "../../components/ui/card";
+import { Checkbox } from "../../components/ui/checkbox";
+import { Input } from "../../components/ui/input";
+import { Label } from "../../components/ui/label";
 
 import {
   Table,
